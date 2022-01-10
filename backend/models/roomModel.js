dd79e3d41bce3dc8cff147ebf500749321d71932
shalinibalodi <shalini.balodi_ug23@ashoka.edu.in>
2022-01-10 15:05:36 +0530

    Backend user and password authentication

--- backend/models/roomModel.js
+++ backend/models/roomModel.js
@@ -59,6 +59,12 @@ const roomSchema = mongoose.Schema({
     },
   ],
 
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  
   createdAt: {
     type: Date,
     default: Date.now,
